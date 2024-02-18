8cfce3ab3f8324fc97349e0fc713f3ea35ded5e9
Aditya Raj <adityarajavi05@gmmail.com>
2024-02-18 13:33:11 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
-// import express from "express"
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
-// const app = express()
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`⚙️ Server is running on port ${process.env.PORT}`);
-// });
-
-// export default app
+export { app }
--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,3 @@
-// import mongoose from "mongoose";
-// import app from "../app.js";
-
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
-//         app()
-//         console.log("CONNECTION SUCCESSFUL");
-//     } catch (error) {
-//         console.log("MONGODB connection FAILED ", error);
-//         process.exit(1)
-//     }
-// }
-
-// export default connectDB
-
 
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
